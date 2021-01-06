11c955f32c5ffba4a23b5c37ea61df06fe28d423
Jain <Vishakha.Jain@mastercard.com>
2021-01-06 20:05:02 +0530

    initial commit

---
+++ Mastercard.Developer.XMLSignVerify.Core/XMLSignVerify/Utility/Context/Constants.cs
@@ -0,0 +1,31 @@
+﻿/* 
+ * Copyright (c) 2020 Mastercard 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ *  
+*/
+
+namespace Mastercard.Developer.XMLSignVerify.Core.Utility.Context
+{
+    public class Constants
+    {
+        public string apphdrtagname = "AppHdr";
+        public string documenttagname = "Document";
+        public string dsNs = "http://www.w3.org/2000/09/xmldsig#";
+        public string elemprefix = "urn1";
+        public string namespaceuriapphdr = "urn:iso:std:iso:20022:tech:xsd:head.001.001.01";
+        public string namespaceuridoc = "urn:iso:std:iso:20022:tech:xsd:rain.001.001.01";
+        public string signprefix = "Sgntr";
+    }
+}
